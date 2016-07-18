49fd24cf7e2de5eca37ef2091a226f84cbb3475b
Scott Scholz <scottscholzpdx@gmail.com>
2016-07-17 18:53:58 -0700

    css addidtions ot base project and major annotations added to scripts.js and index.html. First fully working revision.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
   $("form#vacationWizard").submit(function(event) {
+    var name = $("input#name").val();
     var budget = parseInt($("input#budget").val());
     var climate = $("select#climate").val();
     var social = $("select#social").val();
-    //parameters for wizard
+    //variables for wizard
 
     if (budget) {
       var match; // match is main variable. Using budget, climate and social from above inputs (via forms) and setting parameters via && <= >= !== === < else and if.
@@ -12,59 +13,60 @@ $(document).ready(function() {
         $("#photo").after("<img src='img/phoenix.jpg'>");
         //location photo for the user
         alert('Loading Vacation Match... Press OK to continue!');
-        //simulating computer thinking and wait times, giving the user the impression that the computer had to have time to think to come up wit the best result.
-        alert(match);
-        //text alert stating the variable match
+        //simulating computer thinking and wait times, giving the user the impression that the computer had to have time to think to come up with the best result.
+        alert(name + ", It looks like you're going to have a great trip to " + match + ". Enjoy the blistering heat and sunshine because you can get all of it here!");
+        //text alert stating the name of user, variable match, and personalized message.
         $(this).remove();
-        //removes form entries (to prevent duplicates entries) and shows location picture in the span where the form was.
+        //removes form entries (to prevent duplicates entries) and shows location picture in the span where the form was. simulaton of navigating to a destination webpage.
       }
       if (climate === 'hot' && budget > 800 && social !== 'party') {
         match ="Maui!";
         $("#photo").after("<img src='img/maui.jpg'>");
         alert('Loading Vacation Match... Press OK to continue!');
-        alert(match);
+        alert(name + ", It looks like you're going to have a great trip to " + match + ". Enjoy the beaches and sunshine because this is as good as it gets!");
         $(this).remove();
       }
       if (climate === 'cold' && budget <= 800 && budget >= 250 && social !== 'party') {
         match ="Anchorage";
         $("#photo").after("<img src='img/anchorage.jpg'>");
         alert('Loading Vacation Match... Press OK to continue!');
-        alert(match);
+        alert(name + ", It looks like you're going to have a great trip to " + match + ". You wanted cold and there's a lot of it here!");
         $(this).remove();
       }
       if (climate === 'cold' && budget > 800 && social !== 'party') {
         match ="Iceland";
         $("#photo").after("<img src='img/iceland.jpg'>");
         alert('Loading Vacation Match... Press OK to continue!');
-        alert(match);
+        alert(name + ", It looks like you're going to have a great trip to " + match + ". Enjoy all that Iceland has to offer!");
         $(this).remove();
       }
       if (budget < 250) {
         match ="Portland Staycation!";
         $("#photo").after("<img src='img/portland.jpg'>");
         alert('Loading Vacation Match... Press OK to continue!');
-        alert(match);
+        alert(name + ", It looks like you're going to have a great trip doing a " + match + ". At least you're not having to deal with the airport!");
         $(this).remove();
       }
       if (budget >=250 && social === 'party') {
         match ="Las Vegas";
         $("#photo").after("<img src='img/vegas.jpg'>");
         alert('Loading Vacation Match... Press OK to continue!');
-        alert(match);
+        alert(name + ", It looks like you're going to have a great trip to " + match + ". Enjoy the nightlife but stay out of trouble!");
         $(this).remove();
+        // console.log(match);
       }
 
     } else {
       alert('Please enter your budget.');
-      //dded so that the form wouldn't submit if there wasn't enough data submitted. urrently the only option that isn't pre-selected is the budget string input. this was effectively rendered obsolete when "required=""" was added into the form class divider.
+      //added so that the form wouldn't submit if there wasn't enough data submitted. currently the only option that isn't pre-selected is the budget string input. this was effectively rendered obsolete when "required=""" was added into the form class divider. If the form is changed this will still work and prevent the user from submitting incomplete information.
     }
-
 
     console.log(budget);
     console.log(social);
     console.log(climate);
     console.log(match);
-    //console log variable checkers for troubleshooting issues. JShints shows that console.log(match); is wrong (line 66), but it shows up just fine in chrome console log. this may be inadvertintly be an error in JShints as match isn't explicitly defined until the user uses the form inputs, so logically match isn't defined YET.
+    console.log(name);
+    //console log variable checkers for troubleshooting issues. JShints (an atom editor add-on) shows that console.log(match); is wrong (line 66), as does Chrome console log. this may be inadvertintly be an error in JShints as match isn't explicitly defined until the user uses the form inputs, so logically match isn't defined YET. When defining match and console.log(match) is called upon (line 56) it does work. I believe this is known as local scope, as opposed to variable/global scope.
 
     $("#vaca").show();
     event.preventDefault();
